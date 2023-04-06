bad48f2e0bf9e9a3b5073edbe911cf3c37382da8
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-04-06 10:28:44 +0200

    Created the model and controller for the register user

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONs  */
 
@@ -36,3 +37,20 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/* ROUTES */
+
+app.post("/auth/register/", upload.single("picture"), register);
+
+/* MONGOOSE SETUP */
+
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`SERVER PORT: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
