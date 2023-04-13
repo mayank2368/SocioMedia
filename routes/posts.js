136a9cbc46f8ad97a075ab71d6ed23085d2d5658
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-04-13 22:30:08 +0200

    created the post models and controllers and updated the naming typo and also added the dummy data of the users

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { getUserFeeds, getUserPosts, likePost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
 /* READ */
-router.get("/", verifyToken, getFeedPosts);
+router.get("/", verifyToken, getUserFeeds);
 router.get("/:id/posts", verifyToken, getUserPosts);
 
 /* UPDATE */
 router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
