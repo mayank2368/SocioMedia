b1a7c0d259288ad643f7445904f4cabefb84d7c7
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-04-19 12:41:30 +0200

    updated controller

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONs  */
 
@@ -64,5 +67,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`SERVER PORT: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
